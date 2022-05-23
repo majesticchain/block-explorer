74a0d42879d3012ab17012b51f68937508e4f2b2
buy-aka <buyka.1776@gmail.com>
2022-05-23 15:08:11 +0800

    update

--- apps/indexer/config/dev.exs
+++ apps/indexer/config/dev.exs
@@ -3,31 +3,31 @@ use Mix.Config
 config :indexer, Indexer.Tracer, env: "dev", disabled?: true
 
 config :logger, :indexer,
-  level: :info,
+  level: :warn,
   path: Path.absname("logs/dev/indexer.log")
 
 config :logger, :indexer_token_balances,
-  level: :info,
+  level: :warn,
   path: Path.absname("logs/dev/indexer/token_balances/error.log"),
   metadata_filter: [fetcher: :token_balances]
 
 config :logger, :failed_contract_creations,
-  level: :info,
+  level: :warn,
   path: Path.absname("logs/dev/indexer/failed_contract_creations.log"),
   metadata_filter: [fetcher: :failed_created_addresses]
 
 config :logger, :addresses_without_code,
-  level: :info,
+  level: :warn,
   path: Path.absname("logs/dev/indexer/addresses_without_code.log"),
   metadata_filter: [fetcher: :addresses_without_code]
 
 config :logger, :pending_transactions_to_refetch,
-  level: :info,
+  level: :warn,
   path: Path.absname("logs/dev/indexer/pending_transactions_to_refetch.log"),
   metadata_filter: [fetcher: :pending_transactions_to_refetch]
 
 config :logger, :empty_blocks_to_refetch,
-  level: :info,
+  level: :warn,
   path: Path.absname("logs/dev/indexer/empty_blocks_to_refetch.log"),
   metadata_filter: [fetcher: :empty_blocks_to_refetch]
 
