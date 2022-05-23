be4cc26b9d78e95d2f061969ea66a948ff793fad
buy-aka <buyka.1776@gmail.com>
2022-05-23 15:05:00 +0800

    change logger level for explorer to warn

--- config/dev.exs
+++ config/dev.exs
@@ -1,10 +1,10 @@
 use Mix.Config
 
 # DO NOT make it `:debug` or all Ecto logs will be shown for indexer
-config :logger, :console, level: :info
+config :logger, :console, level: :warn
 
 config :logger, :ecto,
-  level: :debug,
+  level: :warn,
   path: Path.absname("logs/dev/ecto.log")
 
 config :logger, :error, path: Path.absname("logs/dev/error.log")
